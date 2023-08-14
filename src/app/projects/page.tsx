a256a3d9926e80ef7cbf440a40ee585e2685fe7c
Brandon Corn <cornbrandon@Brandons-MacBook-Pro.local>
2023-08-14 00:18:58 -0600

    created Image gallery images on home page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Projects() {
+  return (
+    <div>
+      <Link href="/"> On the projects page, go home </Link>
+    </div>
+  );
+}
