db9bf351e42ea04b68d806e1086267938fb8e088
Brandon Corn <cornbrandon@Brandons-MacBook-Pro.local>
2023-08-14 00:39:35 -0600

    Revert "created Image gallery images on home page"
    This reverts commit a256a3d9926e80ef7cbf440a40ee585e2685fe7c.

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
     serverActions: true,
   },
   env: {
-    accountSid: "",
-    authToken: "",
-    verifyServiceId: "",
-  },
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, DELETE, PUT, POST",
-  },
-};
+    accountSid: 'AC4e5291dddcf44d59d36745b8440bf34c',
+    authToken: 'e74cf603b0750ec84989d9187f95d89d',
+    verifyServiceId: 'VA14fd414e0d7ebd16010d28717dbc88d1'
+},
+headers:{
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': "GET, DELETE, PUT, POST"
+}
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
