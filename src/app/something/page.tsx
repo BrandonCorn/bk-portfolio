a4b7d2ed5d4860e27c6ddb13d746de6252f1e62a
Brandon Corn <cornbrandon@Brandons-MacBook-Pro.local>
2023-08-14 00:40:28 -0600

    Revert "Revert "created Image gallery images on home page""
    This reverts commit db9bf351e42ea04b68d806e1086267938fb8e088.

---
+++ src/app/something/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Something() {
+  return (
+    <div>
+      <Link href="/"> On the something page, go home </Link>
+    </div>
+  );
+}
