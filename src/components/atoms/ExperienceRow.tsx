25a9a1b837c2f5d355e260ba9077db0235d0ed74
BrandonCorn <cornbrandonk@gmail.com>
2023-10-13 18:42:02 -0600

    Finish draft of experience section

--- src/components/atoms/ExperienceRow.tsx
+++ src/components/atoms/ExperienceRow.tsx
@@ -18,12 +18,12 @@ const ExperienceRow: React.FC<ExperienceRowProps> = ({
 }) => {
   return (
     <div className="flex flex-col md:flex-row my-12 w-5/6 justify-start">
-      <div className="flex w-full justify-start sm:justify-center p-4">
+      <div className="flex w-1/2 md:w-1/3 justify-start md:justify-center p-4">
         <p className="text-sm text-zinc-400">
           {startDate} - {endDate}
         </p>
       </div>
-      <div className="flex flex-col group/item hover:bg-zinc-800 rounded-lg p-4 w-full space-y-2">
+      <div className="flex flex-col group/item hover:bg-zinc-800 rounded-lg p-4 w-full md:w-1/2 space-y-2">
         <h2 className="text-xl font-bold"> {company} </h2>
         <p className="text-md"> {title} </p>
         <p className="text-sm"> {description} </p>
