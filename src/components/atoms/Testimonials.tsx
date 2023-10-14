8c9371d47cd42b33782334cc59a707b4274ad1fd
BrandonCorn <cornbrandonk@gmail.com>
2023-10-14 03:09:10 -0600

    fix issue with double quotes on testimonials

--- src/components/atoms/Testimonials.tsx
+++ src/components/atoms/Testimonials.tsx
@@ -42,7 +42,9 @@ const TestimonialCard = ({
       key={index}
       className="rounded-lg shadow-xl py-6 pr-6 pl-0 mb-8 mr-8 text-zinc-600 dark:text-zinc-400 "
     >
-      <p className="text-white text-xl font-bold mb-2">"{testimonial.quote}"</p>
+      <p className="text-white text-xl font-bold mb-2">
+        &quot;{testimonial.quote}&quot;
+      </p>
       <p className="text-lg mb-1"> {testimonial.name} </p>
       <p className="text-sm mb-1">
         {testimonial.jobTitle} at {testimonial.company}
