0d8b809e1a65eaf48b3cb48a6c02ddc0d5da94d9
BrandonCorn <cornbrandonk@gmail.com>
2023-10-19 08:38:21 -0600

    small footer adjustments

---
+++ src/components/atoms/Titles/BasicTitle/BasicTitle.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import { AtomProps } from "../../types";
+
+export type BasicTitle = {
+  text: string;
+} & AtomProps<"h1">;
+
+const BasicTitle: React.FC<BasicTitle> = ({ text, ...rest }) => {
+  const classes = clsx(
+    "flex text-lg text-black dark:text-white",
+    rest.className
+  );
+  return <h1 className={classes}> {text} </h1>;
+};
+
+export default BasicTitle;
--- src/components/molecules/Footers/Footer/Footer.tsx
+++ src/components/molecules/Footers/Footer/Footer.tsx
@@ -3,72 +3,74 @@ import { FaGithub, FaLinkedin, FaInstagram, FaDiscord } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import DescriptionText from "@/components/atoms/Texts/DescriptionText/DescriptionText";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center m-auto bg-inherit w-screen h-40">
-      <div className="flex justify-between items-center w-3/4 lg:w-1/2 xl:w-1/4">
-        <IconContext.Provider value={{ size: "3em" }}>
-          <Link href="https://github.com/BrandonCorn">
-            <motion.div
-              whileHover={{ scale: 1.2, transition: { duration: 1 } }}
-              className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
-            >
-              <FaGithub />
-              <p> GitHub</p>
-            </motion.div>
-          </Link>
-        </IconContext.Provider>
-        <IconContext.Provider value={{ color: "#0077B5", size: "3em" }}>
-          <Link href="https://www.linkedin.com/in/brandon-corn-3b3249152">
-            <motion.div
-              whileHover={{ scale: 1.2, transition: { duration: 1 } }}
-              className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
-            >
-              <FaLinkedin />
-              <p> LinkedIn</p>
-            </motion.div>
-          </Link>
-        </IconContext.Provider>
-        <IconContext.Provider
-          value={{
-            color: "",
-            size: "3em",
-            style: {
-              background:
-                "linear-gradient(to bottom , #405DE6, #5B51D8,#833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #F77737, #FCAF45, #FFDC80)",
-              borderRadius: "1em",
-            },
-          }}
-        >
-          <Link href="https://instagram.com/thatscornwithac">
-            <motion.div
-              whileHover={{ scale: 1.2, transition: { duration: 1 } }}
-              className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
-            >
-              <FaInstagram />
-              <p> Instagram</p>
-            </motion.div>
-          </Link>
-        </IconContext.Provider>
-        <IconContext.Provider
-          value={{
-            color: "",
-            size: "3em",
-            style: { backgroundColor: "#5865F2" },
-            className: "rounded-md",
-          }}
-        >
-          <Link href="https://discord.com/users/thatscornwithac">
-            <motion.div
-              whileHover={{ scale: 1.2, transition: { duration: 1 } }}
-              className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
-            >
-              <FaDiscord />
-              <p> Discord</p>
-            </motion.div>
-          </Link>
-        </IconContext.Provider>
+    <footer className="flex items-center justify-center m-auto bg-transparent w-screen h-24 mt-2">
+      <div className="flex flex-row justify-center items-center border-t pt-8 w-screen">
+        <div className="flex items-center justify-start w-3/4 lg:w-1/2 space-x-8 mb-8">
+          <IconContext.Provider value={{ size: "2em" }}>
+            <Link href="https://github.com/BrandonCorn">
+              <motion.div
+                whileHover={{ scale: 1.2, transition: { duration: 1 } }}
+                className="flex flex-col items-center justify-end space-y-2 cursor-pointer"
+              >
+                <FaGithub />
+              </motion.div>
+            </Link>
+          </IconContext.Provider>
+          <IconContext.Provider value={{ color: "#0077B5", size: "2em" }}>
+            <Link href="https://www.linkedin.com/in/brandon-corn-3b3249152">
+              <motion.div
+                whileHover={{ scale: 1.2, transition: { duration: 1 } }}
+                className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
+              >
+                <FaLinkedin />
+              </motion.div>
+            </Link>
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{
+              size: "2em",
+              style: {
+                background:
+                  "linear-gradient(to bottom , #405DE6, #5B51D8,#833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #F77737, #FCAF45, #FFDC80)",
+                borderRadius: "0.4em",
+              },
+            }}
+          >
+            <Link href="https://instagram.com/thatscornwithac">
+              <motion.div
+                whileHover={{ scale: 1.2, transition: { duration: 1 } }}
+                className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
+              >
+                <FaInstagram />
+              </motion.div>
+            </Link>
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{
+              size: "2em",
+              style: { backgroundColor: "#5865F2" },
+              className: "rounded-md",
+            }}
+          >
+            <Link href="https://discord.com/users/thatscornwithac">
+              <motion.div
+                whileHover={{ scale: 1.2, transition: { duration: 1 } }}
+                className="flex flex-col items-center justify-center space-y-2 cursor-pointer"
+              >
+                <FaDiscord />
+              </motion.div>
+            </Link>
+          </IconContext.Provider>
+        </div>
+        <div>
+          <p className="mb-8 dark:text-gray-600 text-black">
+            &copy; Brandon Corn All Rights Reserved
+          </p>
+        </div>
       </div>
     </footer>
   );
