017830e114c7098d8ae76d5dd5c538e4e1d61d01
BrandonCorn <cornbrandonk@gmail.com>
2024-01-30 19:54:28 -0500

    fix errors with session not needed yet

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,12 +1,7 @@
 "use server";
 import LoginForm from "@/components/molecules/Forms/LoginForm/LoginForm";
-import { getServerSession } from "next-auth";
-import { getProviders } from "next-auth/react";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Page() {
-  const session = await hasSession();
-  console.log("inside page ", session);
   return (
     <>
       <div className="flex flex-shrink">
@@ -15,9 +10,3 @@ export default async function Page() {
     </>
   );
 }
-
-export const hasSession = async () => {
-  const session = await getServerSession(authOptions);
-  console.log("session stuff ", session);
-  return session;
-};
