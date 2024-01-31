a14e5daad47d22951ea5216f6cb330402a2ce963
BrandonCorn <cornbrandonk@gmail.com>
2024-01-30 20:29:50 -0500

    update title for error pages

--- src/app/auth/signup/error.tsx
+++ src/app/auth/signup/error.tsx
@@ -9,7 +9,7 @@ export default function Error({ error, reset }: any) {
 
   return (
     <div className="space-y-4">
-      <h2 className="text-lg font-bold">Error</h2>
+      <h2 className="text-lg font-bold">Something went wrong.</h2>
       <p className="text-sm">{error?.message}</p>
       <div>
         <button onClick={() => reset()}>Try Again</button>
