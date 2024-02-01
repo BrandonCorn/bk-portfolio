edff7234d0d06fb4b16676e18a3e637ecbec0628
BrandonCorn <cornbrandonk@gmail.com>
2024-01-31 22:35:33 -0500

    modification to fix type error with NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-import { authOptions } from "@/lib/nextAuth";
+import NextAuth from "next-auth";
+import authOptions from "@/lib/nextAuth";
 
 
-const handler = authOptions;
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
--- src/lib/nextAuth/index.ts
+++ src/lib/nextAuth/index.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
 import GitHubProvider from "next-auth/providers/github";
@@ -7,7 +7,7 @@ import prisma from '../prismaDb';
 
 
 //custom providers described here https://next-auth.js.org/configuration/providers/credentials
-export const authOptions: NextAuthOptions = NextAuth({
+const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   //configure one or more auth providers
   providers: [
@@ -70,7 +70,9 @@ export const authOptions: NextAuthOptions = NextAuth({
   session: {
     strategy: 'jwt'
   }
-});
+}
+
+export default authOptions;
 
 export type ProviderInfo = {
   name: string;
