98d53957be43c50c6760b6a89692131f39fb6c73
BrandonCorn <cornbrandonk@gmail.com>
2024-02-08 01:23:10 -0500

    disallow button click for sms contact form

--- src/components/organisms/Sections/SmsContactSection/SmsContactSection.tsx
+++ src/components/organisms/Sections/SmsContactSection/SmsContactSection.tsx
@@ -250,7 +250,7 @@ const SmsContactForm = () => {
         FormButton={
           <LoadingButton
             className={
-              "h-10 w-40 bg-sky-500 hover:bg-sky-700 border-sky-500 hover:border-sky-700 text-sm border-4 text-white py-1 px-4 rounded"
+              "h-10 w-40 bg-sky-500 hover:bg-sky-700 border-sky-500 hover:border-sky-700 hover:cursor-not-allowed text-sm border-4 text-white py-1 px-4 rounded"
             }
             isLoading={buttonProps.isLoading}
             text={buttonProps.text}
