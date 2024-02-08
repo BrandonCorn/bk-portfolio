3d33d0a5100f0e0427eaf27a4a7a4fb610ef1b3b
BrandonCorn <cornbrandonk@gmail.com>
2024-02-08 03:03:12 -0500

    fix error with escaped character

--- src/app/auth/loading.tsx
+++ src/app/auth/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <div>We're waiiting</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
 }
