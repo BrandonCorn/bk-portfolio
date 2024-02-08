c81885b298f31a3d60397033c565d6221161d48d
BrandonCorn <cornbrandonk@gmail.com>
2024-02-08 02:03:22 -0500

    add comment button

--- src/components/molecules/Modals/PostModal/PostModal.tsx
+++ src/components/molecules/Modals/PostModal/PostModal.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { FaComment } from "react-icons/fa";
 /**
  * Element for each blog post
  */
@@ -18,20 +20,43 @@ const PostModal = ({
   const [contentChanges, setContentChanges] = useState(formattedPost);
   const [showButtonText, setShowButtonText] =
     useState<ShowButtonText>("show more");
+  const [isUser, setIsUser] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false);
 
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setIsUser(true);
+    }
+  }, [status]);
+
+  /**
+   * Based on what the button text is, decide whether to show the whole blog post or less of it's text
+   */
   const changeContentWordCount = () => {
     if (showButtonText === "show more") setContentChanges(content);
     else setContentChanges(formattedPost);
   };
-
+  /**
+   * Change button text to opposite of current value
+   */
   const changeShowButtonText = () => {
     if (showButtonText === "show more") setShowButtonText("show less");
     else setShowButtonText("show more");
   };
 
+  /**
+   * Handler function for showing all or less of blog post content
+   */
   const handleShowContent = () => {
     changeContentWordCount();
     changeShowButtonText();
+  };
+
+  const handleShowCommentArea = () => {
+    setShowCommentBox(true);
+    alert("what s up yo");
   };
 
   return (
@@ -45,13 +70,29 @@ const PostModal = ({
       <article className="px-4 py-2 text-wrap">
         <p className=" text-gray-700 text-md">{contentChanges}</p>
       </article>
-      <div className="text-center">
+      <div className="py-2 text-center">
         <button
           className="text-purple-500  font-light hover:opacity-90"
           onClick={handleShowContent}
         >
           {showButtonText}
         </button>
+      </div>
+      <div className={`pt-3 pl-2 border-t-2 w-full text-left hover:opacity-90`}>
+        <div
+          className={`flex w-fit ${
+            isUser ? "hover:cursor-pointer" : "hover:cursor-not-allowed"
+          }`}
+          onClick={handleShowCommentArea}
+        >
+          <FaComment className="text-purple-600 text-xl" />
+          <button
+            disabled={isUser ? false : true}
+            className={`text-purple-500 pl-2`}
+          >
+            Comment
+          </button>
+        </div>
       </div>
     </div>
   );
